2a19fa8a9d0afecd8be6118d13dc7a00771aa924
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-09-18 15:38:20 -0600

    Variables and console

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿main(args);
+
+static void main(string[] args)
+{
+    Bumble myBee = new Bumble();
+    myBee.echo();
+}
+
+class Bumble
+{
+    public void echo()
+    {
+        string? x = Console.ReadLine();
+        Console.WriteLine($"{x} bzzzz!");
+    }
+    public void print(string arg)
+    {
+        Console.WriteLine("bzzzzzzzzz!");
+        Console.WriteLine(arg);
+    }
+}
