67706a89be36476f0dfc07c9b7766beb7f522a51
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-09-19 13:17:05 -0600

    strings and chars

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,9 +1,11 @@
-﻿main(args);
+﻿using System.Security.Cryptography.X509Certificates;
+
+main(args);
 
 static void main(string[] args)
 {
     Bumble myBee = new Bumble();
-    myBee.maxValues();
+    myBee.clickClack();
 }
 
 class Bumble
@@ -61,4 +63,80 @@ class Bumble
         Console.WriteLine(ushort.MaxValue);
         Console.WriteLine(ulong.MaxValue);
     }
+
+    public void infinity()
+    {
+        double x = Math.Pow(9999,99999999999);
+        Console.WriteLine(double.IsInfinity(x));
+    }
+
+    public void round()
+    {
+        double a = 50.5;
+        double b = 51.5;
+        Console.WriteLine($"A={Math.Round(a)} B={Math.Round(b)}");
+        Console.WriteLine($"A={Math.Round(a, 0, MidpointRounding.AwayFromZero)} B={Math.Round(b, 0, MidpointRounding.AwayFromZero)}");
+    }
+
+    public void Char()
+    {
+        char a = 'a';
+        int unicodeMax = 65536;
+        int asciiMax = 255;
+        for( int i = 0; i < asciiMax; i++)
+        {
+            a = (char)(i);
+            Console.WriteLine($"{i} = {a}|");
+        }
+        
+    }
+
+    public void Hearts()
+    {
+
+        char[] characters = new char[181];
+
+        for(int i = 0;i < 180; i = i + 2)
+        {
+            characters[i] = (char)3;
+            characters[i+1] = (char)32;
+        }
+        characters[180]= (char)10;
+        string hearts = new string(characters);
+        Console.Write(hearts);
+    }
+
+    public void backslash()
+    {
+        string a = "\'";
+        string b = "\"";
+        string c = "\\";
+        string d = "\0";
+        string e = "\a";
+        string f = "\b";
+        string g = "\f";
+        string h = "\n";
+        string i = "\r";
+        string j = "\t";
+        string k = "\v";
+
+        Console.Write (a);
+        Console.Write (b);
+        Console.Write (c);
+        Console.Write(d);
+        Console.Write (e);
+        Console.Write (f);
+        Console.Write (g);
+        Console.Write (h);
+        Console.Write (i);
+        Console.Write (j);
+        Console.Write (k);
+
+    }
+
+    public void clickClack()
+    {
+        Console.Write("\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a");
+    }
+
 }
